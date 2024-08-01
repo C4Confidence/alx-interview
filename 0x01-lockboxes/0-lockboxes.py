eabcaa74b8a6498e2e69e11d37524ef9c420eced
C4Confidence <iamconfidence5@gmail.com>
2024-08-01 16:47:45 +0100

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True  # The first box is always unlocked
+    keys = set(boxes[0])  # Start with keys from the first box
+
+    while keys:
+        new_keys = set()
+        for key in keys:
+            if key < n and not opened[key]:
+                opened[key] = True
+                new_keys.update(boxes[key])
+        keys = new_keys
+
+    return all(opened)
